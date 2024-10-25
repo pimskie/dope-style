df772bb80ec5c9943de2290273980a5ef10891d8
Pim van Die <pim.vandie@iodigital.com>
2024-10-25 13:27:25 +0200

    category list client component

--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -1,18 +1,29 @@
 "use client";
+import { Skeleton } from "@/components/ui/skeleton";
 
 import type { Category } from "@/types/Category";
 import CategoryCard from "@/components/CategoryCard/CategoryCard";
 import Heading from "@/components/Typography/Heading/Heading";
-
+import styles from "./CategoryList.module.css";
 import { StoreService } from "@/services/store";
 import { useEffect, useState } from "react";
 
 const renderCategories = (categories: Category[]) => {
   return (
-    <div className="categories-container grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className={styles.categoriesContainer}>
       {categories.map((category) => (
         <CategoryCard {...category} key={category.name} />
       ))}
+    </div>
+  );
+};
+
+const renderSkeleton = () => {
+  return (
+    <div className={styles.categoriesContainer}>
+      <Skeleton className="w-full h-[300px]" />
+      <Skeleton className="w-full h-[300px]" />
+      <Skeleton className="w-full h-[300px]" />
     </div>
   );
 };
@@ -35,7 +46,7 @@ const CategoryList = () => {
   return (
     <div>
       <Heading Tag="h2">Categories</Heading>
-      {isLoading ? <div>Loading...</div> : renderCategories(categories)}
+      {isLoading ? renderSkeleton() : renderCategories(categories)}
     </div>
   );
 };
