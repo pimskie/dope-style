7afcc8bf3b0a8578891c1d65a3573cf8cdbd74d5
Pim van Die <pim.vandie@iodigital.com>
2024-10-25 13:36:35 +0200

    revert category stuff loading on client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,24 @@ import CategoryList from "@/components/CategoryList/CategoryList";
 import Heading from "@/components/Typography/Heading/Heading";
 import Leader from "@/components/Typography/Leader/Leader";
 
+type CategoryList = Array<Category>;
+
 const App = async () => {
+  const categories = await StoreService.category.getAll();
+
   return (
     <div>
       <Heading>Dope Style</Heading>
 
       <Leader>
-        Welcome to Dope Style, where streetlife meets fashion. Our collection is
+        Welcome to Cool Style, where streetlife meets fashion. Our collection is
         a tribute to the streets, featuring edgy streetwear, durable skatewear,
         and top-notch graffiti supplies that define your unique style. Whether
-        you're hitting the pavement or creating your next masterpiece, Dope
+        you're hitting the pavement or creating your next masterpiece, Cool
         Style has got you covered. Embrace the culture, stand out, and make your
-        mark with Dope Style.
+        mark with Cool Style.
       </Leader>
-
-      <CategoryList />
+      <CategoryList categories={categories} />
     </div>
   );
 };
--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -1,52 +1,19 @@
-"use client";
-import { Skeleton } from "@/components/ui/skeleton";
-
 import type { Category } from "@/types/Category";
 import CategoryCard from "@/components/CategoryCard/CategoryCard";
 import Heading from "@/components/Typography/Heading/Heading";
-import styles from "./CategoryList.module.css";
-import { StoreService } from "@/services/store";
-import { useEffect, useState } from "react";
 
-const renderCategories = (categories: Category[]) => {
-  return (
-    <div className={styles.categoriesContainer}>
-      {categories.map((category) => (
-        <CategoryCard {...category} key={category.name} />
-      ))}
-    </div>
-  );
-};
-
-const renderSkeleton = () => {
-  return (
-    <div className={styles.categoriesContainer}>
-      <Skeleton className="w-full h-[300px]" />
-      <Skeleton className="w-full h-[300px]" />
-      <Skeleton className="w-full h-[300px]" />
-    </div>
-  );
-};
-
-const CategoryList = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [categories, setCategories] = useState<Category[]>([]);
-
-  useEffect(() => {
-    const fetchCategories = async () => {
-      const result = await StoreService.category.getAll();
-
-      setCategories(result);
-      setIsLoading(false);
-    };
-
-    fetchCategories();
-  }, []);
-
+interface Props {
+  categories: Array<Category>;
+}
+const CategoryList = ({ categories }: Props) => {
   return (
     <div>
       <Heading Tag="h2">Categories</Heading>
-      {isLoading ? renderSkeleton() : renderCategories(categories)}
+      <div className="categories-container grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        {categories.map((category) => (
+          <CategoryCard {...category} key={category.name} />
+        ))}
+      </div>
     </div>
   );
 };
