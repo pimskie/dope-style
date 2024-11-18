773c25b7634b24a47633da3d96294e02814772d9
Pim van Die <pim.vandie@iodigital.com>
2024-11-18 10:22:05 +0100

    added pagination

--- src/components/CategoryOverview/CategoryOverview.tsx
+++ src/components/CategoryOverview/CategoryOverview.tsx
@@ -5,15 +5,24 @@ import styles from "./CategoryOverview.module.css";
 import VerticalStack from "../Layout/Stack/VerticalStack";
 import { CategoryHeader } from "./CategoryHeader";
 import ProductList from "../ProductList/ProductList";
-import { useEffect, useCallback, useState } from "react";
+import { useEffect, useCallback, useState, useMemo } from "react";
 import { Product } from "@/types/Product";
 import { StoreService } from "@/services/store";
+
+import { useSearchParams } from "next/navigation";
+import { usePaginatedResults } from "@/hooks/usePaginatedResults";
+import DopePagination from "@/components/Pagination/Pagination";
+
+const perPage = 6;
 
 const CategoryOverview = () => {
   const { category } = useCategory();
 
   const [isLoading, setIsLoading] = useState(true);
   const [products, setProducts] = useState<Product[] | undefined>([]);
+
+  const params = useSearchParams();
+  const page = params.has("page") ? parseInt(params.get("page")!, 10) : 1;
 
   const fetchProducts = useCallback(async () => {
     const products = await StoreService.product.where([
@@ -30,17 +39,21 @@ const CategoryOverview = () => {
 
   useEffect(() => {
     fetchProducts();
-  }, [fetchProducts]);
+  }, []);
+
+  const productsSubset = usePaginatedResults(products, page);
 
   return (
     <div className={styles.overview}>
       <VerticalStack>
         <CategoryHeader category={category} />
         <ProductList
-          products={products}
+          products={productsSubset()}
           categorySlug={category.slug}
           isLoading={isLoading}
         />
+
+        <DopePagination currentPage={page} itemsCount={products?.length ?? 0} />
       </VerticalStack>
     </div>
   );
